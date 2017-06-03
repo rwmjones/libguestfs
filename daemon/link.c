e5bafaf1a7978ec64693524ed5a6f5600f8792df
Richard W.M. Jones <rjones@redhat.com>
2017-06-03 14:56:57 +0100

    daemon: Reimplement ‘readlink’ API in OCaml.

--- daemon/link.c
+++ daemon/link.c
@@ -32,22 +32,6 @@
 
 GUESTFSD_EXT_CMD(str_ln, ln);
 
-char *
-do_readlink (const char *path)
-{
-  char *link;
-
-  CHROOT_IN;
-  link = areadlink (path);
-  CHROOT_OUT;
-  if (link == NULL) {
-    reply_with_perror ("%s", path);
-    return NULL;
-  }
-
-  return link;			/* caller frees */
-}
-
 char **
 do_internal_readlinklist (const char *path, char *const *names)
 {
